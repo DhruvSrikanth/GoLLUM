6161bf09b6ac497918a5ceb8652f87637ee30b75
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-01-27 01:14:42 -0600

    command line parser

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <p align="center">
 <a>
-    <img src="https://readme-typing-svg.demolab.com?font=Georgia&size=28&duration=3500&pause=2000&multiline=true&width=1000&height=100&lines=GoLLUM - Go + Lite + Language + Understanding + Machine" alt="Typing SVG" />
+    <img src="https://readme-typing-svg.demolab.com?font=Georgia&size=28&duration=3500&pause=2000&multiline=true&width=1000&height=80&lines=GoLLUM - Go + Lite + Language + Understanding + Machine" alt="Typing SVG" />
 </a>
 <br/>
 
---
+++ cmdline/cmdline.go
@@ -0,0 +1,29 @@
+package cmdline
+
+import (
+	"os"
+)
+
+type Cmdline struct {
+	Program string
+	Args    []string
+}
+
+// Returns a new Cmdline struct with the program name and arguments
+func newCmdline(program string, args []string) *Cmdline {
+	return &Cmdline{
+		Program: program,
+		Args:    args,
+	}
+}
+
+// ReadCmdline reads the kernel command line and returns a Cmdline struct.
+func ReadCmdline() *Cmdline {
+	programFile := os.Args[0]
+
+	Args := os.Args[1:]
+
+	cmdline := newCmdline(programFile, Args)
+
+	return cmdline
+}
