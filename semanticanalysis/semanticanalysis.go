f22b32a2129a8848734bc596c946ad4616c0e9fc
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-02-05 19:36:39 -0600

    Add semantic analysis stage

---
+++ semanticanalysis/semanticanalysis.go
@@ -0,0 +1,36 @@
+package semanticanalysis
+
+import (
+	"golite/ast"
+	st "golite/symboltable"
+)
+
+// Check if there are any errors in the error list
+func hasErrors(errors []string) bool {
+	return len(errors) > 0
+}
+
+func reportErrors(errors []string) {
+	for _, err := range errors {
+		println(err)
+	}
+}
+
+func Execute(program *ast.Program) *st.SymbolTables {
+
+	//Define the compiler symbol tables
+	tables := st.NewSymbolTables()
+
+	//First Build the Symbol Table(s) for all global declarations
+	program.BuildSymbolTable(tables)
+
+	//Second perform type checking.
+	errors := make([]string, 0)
+	errors = program.TypeCheck(errors, tables)
+	if hasErrors(errors) {
+		reportErrors(errors)
+		return nil
+	}
+
+	return tables
+}
