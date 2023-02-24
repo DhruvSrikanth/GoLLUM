0933177f61194ed4039f02c51ae88e8596e121b3
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-02-23 23:26:53 -0600

    Add c runtime declarations for LLVM

---
+++ runtime/c.go
@@ -0,0 +1,29 @@
+package runtime
+
+type runtime struct {
+	cFuncs map[string]string
+}
+
+// NewRuntime returns a new runtime
+func NewRuntime() *runtime {
+	cFuncs := make(map[string]string)
+	cFuncs["malloc"] = "i8* @malloc(i32)"
+	cFuncs["free"] = "void @free(i8*)"
+	cFuncs["printf"] = "i32 @printf(i8*, ...)"
+	cFuncs["scanf"] = "i32 @scanf(i8*, ...)"
+	return &runtime{cFuncs: cFuncs}
+}
+
+// GetCFuncs returns a particular C function used by the runtime
+func (r *runtime) GetCFunc(funcKey string) string {
+	return "declare " + r.cFuncs[funcKey]
+}
+
+// GetCFuncs returns the C functions used by the runtime
+func (r *runtime) GetCFuncs() []string {
+	fNames := make([]string, 0)
+	for _, name := range r.cFuncs {
+		fNames = append(fNames, name)
+	}
+	return fNames
+}
