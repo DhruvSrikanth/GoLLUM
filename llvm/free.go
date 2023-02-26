9b0685bc8a65a386d9f993d707f2ad2bfbfda69c
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-02-26 00:51:41 -0600

    Added free llvm IR

---
+++ llvm/free.go
@@ -0,0 +1,61 @@
+package llvm
+
+import (
+	"bytes"
+	"strconv"
+)
+
+// Representation of a free runtime call
+type Free struct {
+	blockLabel      string
+	sourceRegisters []int
+	targetRegisters []int
+}
+
+// NewFree returns a new free runtime call
+func NewFree() *Free {
+	srcR := make([]int, 0)
+	// This is the most recent register used
+	mostRecentR, _ := strconv.Atoi(GetPreviousRegister()[2:])
+	srcR = append(srcR, mostRecentR)
+	tgtR := make([]int, 0)
+	nextR, _ := strconv.Atoi(GetNextRegister()[2:])
+	tgtR = append(tgtR, nextR)
+	return &Free{"", srcR, tgtR}
+}
+
+// String representation of the Local declaration
+func (f *Free) String() string {
+	var out bytes.Buffer
+	// Format is - call void @free(i8* %reg_var)
+	// Common for all
+	out.WriteString("call void @free(i8* %")
+	out.WriteString("r" + strconv.Itoa(f.sourceRegisters[len(f.sourceRegisters)-1]))
+	out.WriteString(")")
+	return out.String()
+}
+
+// Get the registers targeted by the instruction.
+func (f *Free) GetTargets() []int {
+	return f.targetRegisters
+}
+
+// Get the source registers of the instruction.
+func (f *Free) GetSources() []int {
+	return f.sourceRegisters
+}
+
+// Get the immediate value of the instruction.
+func (f *Free) GetImmediate() int {
+	return 0
+}
+
+// Get the label that marks this instruction in code.
+func (f *Free) GetLabel() string {
+	return f.blockLabel
+}
+
+// Set the label that marks this instruction in code.
+func (f *Free) SetLabel(newLabel string) {
+	f.blockLabel = newLabel
+}
