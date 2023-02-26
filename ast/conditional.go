ca1aaa64dd0abaf2088829f7bee65b749f69a217
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-02-25 22:57:28 -0600

    conditional cfg

--- ast/conditional.go
+++ ast/conditional.go
@@ -95,6 +95,29 @@ func (c *Conditional) GetControlFlow(errors []*SemanticAnalysisError, funcEntry 
 
 // Translate the conditional node to LLVM IR
 func (c *Conditional) ToLLVMCFG(tables *st.SymbolTables, blocks []*llvm.BasicBlock, funcEntry *st.FuncEntry) []*llvm.BasicBlock {
+	// Add new block for the condition
+	condBlock := llvm.NewBasicBlock(llvm.GetNextLabel())
+	// Add the condition instructions to the block
+	blocks = append(blocks, condBlock)
+
+	// Add new block for the then block
+	thenBlock := llvm.NewBasicBlock(llvm.GetNextLabel())
+	// Append the then block to the list of blocks since the last block will be considered for the block to add instructions to
+	blocks = append(blocks, thenBlock)
+	blocks = c.thenBlock.ToLLVMCFG(tables, blocks, funcEntry)
+
+	// Add new block for the else block
+	elseBlock := llvm.NewBasicBlock(llvm.GetNextLabel())
+	// Append the else block to the list of blocks since the last block will be considered for the block to add instructions to
+	blocks = append(blocks, elseBlock)
+	if c.elseBlock != nil {
+		blocks = c.elseBlock.ToLLVMCFG(tables, blocks, funcEntry)
+	}
+
+	// Add new block for canonical form
+	block := llvm.NewBasicBlock(llvm.GetNextLabel())
+	// Add the block to the list of blocks
+	blocks = append(blocks, block)
 
 	return blocks
 }
